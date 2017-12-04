a68e8a9db2111a9faef972c9ef113989cc502af7
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-12-04 07:49:35 +0100

    Add Clang cache files

---
+++ cmake/caches/DebugClang.cmake
@@ -0,0 +1,5 @@
+
+set(CMAKE_C_COMPILER clang CACHE STRING "C compiler")
+set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler")
+set(CMAKE_BUILD_TYPE Debug CACHE STRING "Build type")
+set(MDT_CXX_FLAGS_TO_APPEND "-O3 -Wall -Wextra" CACHE STRING "")
